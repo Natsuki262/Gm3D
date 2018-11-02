aa6ba7bbeedbedfdbf1af92b252663a79f1f480c
DA\nwuser <kosyou555@gmail.com>
2018-11-02 15:29:41 +0900

    【add】敵移動のソースファイル追加（直進のみ）

---
+++ battleCity3D/Assets/SuzkiShota/Script/Enemy.cs
@@ -0,0 +1,37 @@
+﻿/*********************************************
+  敵戦車を処理するプログラム
+**********************************************/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    Rigidbody rb;      //Rigidbody取得用
+    Vector3 position;  //位置
+    float speed;       //速度
+    int vector;        //移動方向
+
+	// Use this for initialization
+	void Start ()
+    {
+        rb = GetComponent<Rigidbody>();
+        position = rb.position;
+        speed = 0.05f;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        EnemyMove();		
+	}
+
+    //----------------------------
+    // 敵戦車を移動させる
+    //----------------------------
+    void EnemyMove()
+    {
+        position += new Vector3(0.0f, 0.0f, speed);  //値を加算
+        rb.position = position;                      //更新
+    }
+}
