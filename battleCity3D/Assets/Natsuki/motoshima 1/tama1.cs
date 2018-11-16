c6f5158b4fb871507ebd51a2e81608c1b1bd62bf
Okui_Hirotoshi <hiro.58.fgl.2525.952@icloud.com>
2018-11-16 16:02:32 +0900

    【add】motosimafolderを追加

---
+++ battleCity3D/Assets/Natsuki/motoshima 1/tama1.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tama1 : MonoBehaviour {
+    [SerializeField]
+    private GameObject cannonObject;
+	// Use this for initialization
+	void Start () {
+    }   
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position += transform.forward * 0.01f;
+    }
+}
